c8e4cd9b17db00ba74596220c3161b7adfea1a43
azige <zzb12@qq.com>
2013-03-03 20:48:56 +0800

    project fix

--- kanjimusou/kanjimusou/Form1.Designer.cs
+++ kanjimusou/kanjimusou/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace kanjimusou
+﻿namespace Kanjimusou
 {
     partial class Form1
     {
--- kanjimusou/kanjimusou/Form1.cs
+++ kanjimusou/kanjimusou/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
-namespace kanjimusou
+namespace Kanjimusou
 {
     public partial class Form1 : Form
     {
