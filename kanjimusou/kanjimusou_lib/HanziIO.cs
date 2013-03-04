d226b0d5a94b78b6b59d28e15d0531c0ebd8e024
kiyoshikazuya <kiyoshikazuya@126.com>
2013-03-04 20:19:21 +0800

    Update

---
+++ kanjimusou/kanjimusou_lib/HanziIO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace Kanjimusou_lib
+{
+    class HanziIO
+    {
+        private XmlDocument xmldoc;
+        private XmlNode xmlnode;
+        private XmlElement xmlelem;
+
+        public HanziIO()
+        {
+        } 
+
+        public void OpenFile ( String filepath )
+        {
+            xmldoc = new XmlDocument();
+            xmldoc.Load(filepath);
+
+
+        }
+
+        public bool SaveFile(String filepath, List<Hanzi> hanziList)
+        {
+
+            return true;
+        }
+
+    }
+}
