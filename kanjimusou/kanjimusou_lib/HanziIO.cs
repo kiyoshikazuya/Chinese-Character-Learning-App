053b6325deb858c4dd5448c36dd8d1679fe59824
kiyoshikazuya <kiyoshikazuya@126.com>
2013-03-05 12:31:03 +0800

    Update IO

--- kanjimusou/kanjimusou_lib/HanziIO.cs
+++ kanjimusou/kanjimusou_lib/HanziIO.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Xml;
+using System.Drawing;
 
-namespace Kanjimusou_lib
+namespace Kanjimusou.Lib
 {
-    class HanziIO
+    public class HanziIO
     {
         private XmlDocument xmldoc;
-        private XmlNode xmlnode;
+        private XmlNode xmlnode, root;
         private XmlElement xmlelem;
 
         public HanziIO()
@@ -20,8 +21,94 @@ namespace Kanjimusou_lib
         {
             xmldoc = new XmlDocument();
             xmldoc.Load(filepath);
+            root = xmldoc.SelectSingleNode("/Hanzis");
+        }
+
+        public List<Hanzi> ReadAll()
+        {
+            List<Hanzi> ret = new List<Hanzi>() ;
+            List<Bihua> bihuabiao = new List<Bihua>();
+            List<Point> pt;
+            Hanzi zi;
+            Bihua bi;
+            Point po;
+            String info = "", name = "", path = "";
+            XmlNodeList hzlist = root.ChildNodes, bhlist = null, ptlist = null, tlist;
+
+            foreach (XmlNode xn in hzlist)
+            {
+                bihuabiao = new List<Bihua>();
+                name = xn.Attributes["zi"].Value;
+                tlist = xn.ChildNodes;
+
+                foreach (XmlNode xnn in tlist)
+                {
+                    if (xnn.Name == "bihualist") bhlist = xnn.ChildNodes;
+                    if (xnn.Name == "info") info = xnn.InnerText;
+                    if (xnn.Name == "path") path = xnn.InnerText;
+                }
+                foreach (XmlNode xnn in bhlist)
+                {
+                    ptlist = xnn.ChildNodes;
+                    pt = new List<Point>();
+                    foreach (XmlNode xnnn in ptlist)
+                    {
+                        po = new Point(int.Parse(xnnn.Attributes["x"].Value), int.Parse(xnnn.Attributes["y"].Value));
+                        pt.Add(po);
+                    }
+                    bi = new Bihua(pt);
+                    bihuabiao.Add(bi);
+                }
+                zi = new Hanzi(name, bihuabiao, info, path);
+                ret.Add(zi);
+            }
+
+            return ret;
+        }
+        
+        public Hanzi Read(String zi)
+        {
+            Hanzi ret = null;
+            XmlNodeList hzlist = root.ChildNodes;
+            List<Bihua> bihuabiao = new List<Bihua>();
+            List<Point> pt;
+            Bihua bi;
+            Point po;
+            String info = "", name = "", path = "";
+            XmlNodeList bhlist = null, ptlist = null, tlist;
+
+            foreach (XmlNode xn in hzlist)
+            {
+                if (xn.Attributes["zi"].Value == zi)
+                {
+                    name = xn.Attributes["zi"].Value;
+                    tlist = xn.ChildNodes;
+
+                    foreach (XmlNode xnn in tlist)
+                    {
+                        if (xnn.Name == "bihualist") bhlist = xnn.ChildNodes;
+                        if (xnn.Name == "info") info = xnn.InnerText;
+                        if (xnn.Name == "path") path = xnn.InnerText;
+                    }
+                    foreach (XmlNode xnn in bhlist)
+                    {
+                        ptlist = xnn.ChildNodes;
+                        pt = new List<Point>();
+                        foreach (XmlNode xnnn in ptlist)
+                        {
+                            po = new Point(int.Parse(xnnn.Attributes["x"].Value), int.Parse(xnnn.Attributes["y"].Value));
+                            pt.Add(po);
+                        }
+                        bi = new Bihua(pt);
+                        bihuabiao.Add(bi);
+                    }
+                    ret = new Hanzi(name, bihuabiao, info, path);
+                    break;
+                }
+            }
 
 
+            return ret;    
         }
 
         public bool SaveFile(String filepath, List<Hanzi> hanziList)
