b65faa2c08f0597dbafadf5b892b8bdd8d804a2c
kiyoshikazuya <kiyoshikazuya@126.com>
2013-03-14 22:34:37 +0800

    Update UserIO

---
+++ kanjimusou/kanjimusou_lib/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kanjimusou.Lib
+{
+
+    [Serializable]
+    public class User
+    {
+        private String username;
+
+        public String Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        private String password;
+
+        public String Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        public User(String name, String pass)
+        {
+            this.username = name;
+            this.password = pass;
+        }
+
+    }
+}
