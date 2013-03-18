453f0c68c594151d9f4b67c6c714e9aa3816b6a1
Aoran <330173986@qq.com>
2013-03-18 18:39:49 +0800

    update

--- kanjimusou/kanjimusou/login.Designer.cs
+++ kanjimusou/kanjimusou/login.Designer.cs
@@ -29,9 +29,8 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(login));
             this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.log_in = new System.Windows.Forms.Button();
             this.log_on = new System.Windows.Forms.Button();
             this.name = new System.Windows.Forms.TextBox();
@@ -48,54 +47,41 @@
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(47, 108);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(29, 12);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "账号";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(47, 183);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(29, 12);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "密码";
-            // 
             // log_in
             // 
-            this.log_in.Location = new System.Drawing.Point(49, 240);
+            this.log_in.BackColor = System.Drawing.Color.Transparent;
+            this.log_in.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("log_in.BackgroundImage")));
+            this.log_in.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.log_in.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.log_in.Location = new System.Drawing.Point(287, 208);
             this.log_in.Name = "log_in";
-            this.log_in.Size = new System.Drawing.Size(69, 31);
+            this.log_in.Size = new System.Drawing.Size(46, 29);
             this.log_in.TabIndex = 4;
-            this.log_in.Text = "登陆";
-            this.log_in.UseVisualStyleBackColor = true;
+            this.log_in.UseVisualStyleBackColor = false;
             this.log_in.Click += new System.EventHandler(this.log_in_Click);
             // 
             // log_on
             // 
-            this.log_on.Location = new System.Drawing.Point(202, 241);
+            this.log_on.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("log_on.BackgroundImage")));
+            this.log_on.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.log_on.Location = new System.Drawing.Point(223, 208);
             this.log_on.Name = "log_on";
-            this.log_on.Size = new System.Drawing.Size(67, 30);
+            this.log_on.Size = new System.Drawing.Size(45, 29);
             this.log_on.TabIndex = 5;
-            this.log_on.Text = "注册";
             this.log_on.UseVisualStyleBackColor = true;
             this.log_on.Click += new System.EventHandler(this.log_on_Click);
             // 
             // name
             // 
-            this.name.Location = new System.Drawing.Point(97, 99);
+            this.name.BackColor = System.Drawing.Color.White;
+            this.name.Location = new System.Drawing.Point(71, 159);
             this.name.Name = "name";
             this.name.Size = new System.Drawing.Size(136, 21);
             this.name.TabIndex = 6;
             // 
             // passwd
             // 
-            this.passwd.Location = new System.Drawing.Point(97, 174);
+            this.passwd.Location = new System.Drawing.Point(71, 187);
             this.passwd.Name = "passwd";
             this.passwd.PasswordChar = '*';
             this.passwd.Size = new System.Drawing.Size(136, 21);
@@ -109,13 +95,13 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(310, 306);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(390, 249);
             this.Controls.Add(this.passwd);
             this.Controls.Add(this.name);
             this.Controls.Add(this.log_on);
             this.Controls.Add(this.log_in);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "login";
@@ -128,8 +114,6 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button log_in;
         private System.Windows.Forms.Button log_on;
         private System.Windows.Forms.TextBox name;
