3c5dc4c3da90478bb8f53b36ec3334155045febf
kiyoshikazuya <kiyoshikazuya@126.com>
2013-03-18 21:04:21 +0800

    Update Sound

--- kanjimusou/kanjimusou_lib/util/Sound.cs
+++ kanjimusou/kanjimusou_lib/util/Sound.cs
@@ -2,10 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Media;
 
 namespace Kanjimusou.Lib.util
 {
-    class Sound
+    public static class Sound
     {
+        private static SoundPlayer sePlayer = new SoundPlayer();
+        private static SoundPlayer bgmPlayer = new SoundPlayer();
+
+        private static readonly Dictionary<String, String> fileDic = new Dictionary<string,string>();
+        private const String path = "./resource/sound/";
+
+        static Sound()
+        {
+            fileDic.Add("se1","11.wav");
+        }
+
+
+        public static void PlaySE(String key)
+        {
+            sePlayer.SoundLocation = path + fileDic[key];
+            sePlayer.Play();
+        }
+
+        public static void PlayBGM(String key)
+        {
+            bgmPlayer.SoundLocation = path + fileDic[key];
+            bgmPlayer.PlayLooping();
+        }
+
+
     }
 }
